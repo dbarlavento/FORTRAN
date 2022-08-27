1a33542f26dcd36df416863541239a4dbab90f2a
dbarlavento <nomail@email.com>
2022-08-27 09:24:44 -0300

    matriz_02.f90: adição e multiplicação de matrizes

--- introducao/matriz_02.f90
+++ introducao/matriz_02.f90
@@ -1,4 +1,4 @@
-! Matrizes.
+! Soma e multiplicação de matrizes.
 
 program matrizes
 	implicit none
@@ -7,39 +7,18 @@ program matrizes
 	integer :: i
 
 	! declara uma matriz na própria váriavel
+	integer :: mtz1(10)
 	integer :: mtz2(10)
+	integer :: dobro(10)
+	integer :: quadrados(10)
 
-	! umas matrizes bidimencionais
-	real, dimension(10, 10) :: mtz3
-	real :: mtz4(10,10)
+	mtz1 = [(i, i = 1, 10)]
+	mtz2 = mtz1  
+	!mtz2 = mtz1(10:1:-1) 
 
-	! podemos declara uma matriz por meio de indices personalizados
-	real :: mtz5(0:9)
-	real :: mtz6(-5:5)
+	dobro = mtz1 + mtz2
+	quadrados = mtz1 * mtz2
 
-	! atribuição de valores a uma matriz, fácil assim.
-	mtz1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-	! tambem permite usar um loop implicito
-	mtz2 = [(i, i = 1, 10)]
-
-	! para inicializar com zeros
-	mtz3(:,:) = 0
-
-	! os cinco primeiros da coluna 1 com 1
-	mtz4(1:5,1) = 1
-
-	! e os restantes com 2
-	mtz4(6:, 1) = 2
-
-	! imprimindo ...
-	! os impares
-	print *, mtz1(1:10:2)
-
-	! de trás para frente
-	print *, mtz2(10:1:-1)
-
-	! a coluna 1
-	print *, mtz4(:,1)
-
+	print *, dobro(1:10)
+	print *, quadrados(1:10)
 end program matrizes
